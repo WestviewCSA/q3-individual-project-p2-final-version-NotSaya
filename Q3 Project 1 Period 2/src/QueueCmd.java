ce15e6060fb7fdbb8c39e9b47f433ddf84cd95e3
1906100 <1906100@01-18137.pusd.dom>
2026-02-25 11:59:36 -0800

    started queue command

---
+++ Q3 Project 1 Period 2/src/QueueCmd.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class QueueCmd {
+
+	public static void main(String[][] args) {
+		Queue<String> queue = new LinkedList();
+		
+		
+
+	}
+
+}
